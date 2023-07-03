815f1e1f9feab78cf8fb42587daf7455c546b7cc
TJ Stifter <tjstifter@gmail.com>
2023-07-03 16:47:20 -0400

    passes test

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    for n in range(length) :
+        if(n < 2) :
+            fib.append(n)
+        else :
+            fib.append(fib[n-1] + fib[n-2])
+    print(fib)
